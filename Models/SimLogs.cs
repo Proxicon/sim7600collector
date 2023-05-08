cac9e54e6b723cc94f2327597aa45ccb2402b61a
Konrad Tjaden <konrad.tjaden@computacenter.com>
2023-05-08 09:47:33 +0200

    - Added createdAt for Simdata,Simlogs - Added gpstring split & binding

--- Models/SimLogs.cs
+++ Models/SimLogs.cs
@@ -1,16 +1,25 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
-namespace sim7600collector.Models;
-
-public class SimLogs
+namespace sim7600collector.Models
 {
-    [Key]
-    [Required]
-    public int Id { get; set; }
-    [Required]
-    public string? Device { get; set; }
-    [Required]
-    public string? Logitem { get; set; }
-    [Required]
-    public string? Message { get; set; }
+    public class SimLogs
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        public string? Device { get; set; }
+
+        [Required]
+        public string? Logitem { get; set; }
+
+        [Required]
+        public string? Message { get; set; }
+
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public DateTime CreatedAt { get; set; }
+    }
 }
