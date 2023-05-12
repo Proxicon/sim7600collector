38fc71833b6115a3dd528408d52d5bcd6a806f39
Konrad Tjaden <konrad.tjaden@computacenter.com>
2023-05-12 21:51:20 +0200

    - Set SimData types - Added GPS Utils - Expanded Simdata class

--- Models/DTOs/SimDataDto.cs
+++ Models/DTOs/SimDataDto.cs
@@ -9,19 +9,21 @@ public class SimDataDto
     public string? Location { get; set; }
     public string? Latitude { get; set; }
     public string? Longitude { get; set; }
-    public string? Date { get; set; }
-    public string? UTCTime { get; set; }
-    public string? Altitude { get; set; }
-    public string? Speed { get; set; }
-    public string? Course { get; set; }
-    public string? Battery { get; set; }
-    public int Signal { get; set; }
+    public double? DecimalLatitude { get; set; }
+    public double? DecimalLongitude { get; set; }
+    public DateTime? Date { get; set; }
+    public TimeSpan? UTCTime { get; set; }
+    public double? Altitude { get; set; }
+    public double? SpeedKnots { get; set; }
+    public double? SpeedKmph { get; set; }
+    public double? SpeedMph { get; set; }
+    public double? Course { get; set; }
 
     public SimDataDto() { }
 
     public SimDataDto(SimData Sim7600DataItem) =>
-    (Id, Device, Location ,Latitude, Longitude, Date, UTCTime, Altitude, Speed, Course, Battery, Signal) =
+    (Id, Device, Location ,Latitude, Longitude, Date, UTCTime, Altitude, SpeedKnots, SpeedKmph, SpeedMph, Course) =
     (Sim7600DataItem.Id, Sim7600DataItem.Device, Sim7600DataItem.Location, Sim7600DataItem.Latitude, Sim7600DataItem.Longitude,
-     Sim7600DataItem.Date, Sim7600DataItem.UTCTime, Sim7600DataItem.Altitude, Sim7600DataItem.Speed,
-     Sim7600DataItem.Course, Sim7600DataItem.Battery, Sim7600DataItem.Signal);
+     Sim7600DataItem.Date, Sim7600DataItem.UTCTime, Sim7600DataItem.Altitude, Sim7600DataItem.SpeedKnots,
+     Sim7600DataItem.SpeedKmph, Sim7600DataItem.SpeedMph, Sim7600DataItem.Course);
 }
--- Models/SimData.cs
+++ Models/SimData.cs
@@ -16,13 +16,18 @@ public class SimData
     public string? Location { get; set; }
     public string? Battery { get; set; }
     public int Signal { get; set; }
-    public string Latitude { get; internal set; }
-    public string Longitude { get; internal set; }
-    public string Date { get; internal set; }
-    public string UTCTime { get; internal set; }
-    public string Altitude { get; internal set; }
-    public string Speed { get; internal set; }
-    public string Course { get; internal set; }
+    public string? Latitude { get; internal set; }
+    public string? Longitude { get; internal set; }
+    public Double DecimalLatitude { get; internal set; }
+    public Double DecimalLongitude { get; internal set; }
+    public Double Altitude { get; internal set; }
+    public Double Course { get; internal set; }
+    public Double SpeedKnots { get; internal set; }
+    public Double SpeedKmph { get; internal set; }
+    public Double SpeedMph { get; internal set; }
+    public DateTime Date { get; internal set; }
+    public TimeSpan UTCTime { get; internal set; }
+    public DateTime DateTime { get; internal set; }
 
     [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public DateTime CreatedAt { get; set; }
